cad6c024a6f4b9bdfb84b9bf24e5086074fe83f5
guillermo.sainz <guillermo.sainz@live-u-tad.com>
2017-03-28 17:53:23 +0200

    2

---
+++ iosSwift/iosSwift/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  iosSwift
+//
+//  Created by Guillermo Rodríguez Sainz on 28/3/17.
+//  Copyright © 2017 Guillermo Rodríguez Sainz. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
